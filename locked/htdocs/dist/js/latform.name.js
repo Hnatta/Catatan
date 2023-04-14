cbff2fc9eb4f6d3278cb9901c34ddf4270087f5c
Hnatta <66830227+Hnatta@users.noreply.github.com>
2023-04-14 17:01:33 +0700

    Delete locked directory

--- locked/htdocs/dist/js/latform.name.js
+++
@@ -1,5 +0,0 @@
-<!-- JavaScript -->
-<script>
-function postLock(){if(document.getElementById("lockerPassword").value==="locked"){document.getElementById("lockerContent").classList.remove("contentHide");document.getElementById("postLocker").classList.add("contentHide")}else{alert("Wrong Password!");lockerPassword.setSelectionRange(0,lockerPassword.value.length)}return false}
-</script>
-<!-- End JavaScript -->
